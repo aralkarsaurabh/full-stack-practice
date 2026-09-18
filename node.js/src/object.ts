8b160cd378f0a858d477a083da0f574be2215962
Saurabh Aralkar <saurabharalkar@gmail.com>
2026-09-18 07:17:07 +0530

    feat: add types and example objects for practice questions Q6 through Q15

--- node.js/src/object.ts
+++ node.js/src/object.ts
@@ -139,18 +139,100 @@ console.log('-----------------------------------------------------------------')
 // Include accountHolder, balance, and accountType.
 // Deposit ₹10,000 into the account.
 
+type bankAccount = {
+    accountHolder: string,
+    balance: number,
+    accountType: string
+};
+
+let account_1: bankAccount = {
+    accountHolder: "Saurabh Aralkar",
+    balance: 52000,
+    accountType: "Savings"
+};
+
+console.log(account_1);
+
+account_1.balance = account_1.balance + 10000;
+
+console.log(account_1);
+
+console.log('-----------------------------------------------------------------');
+
 // Q7
 // Create a restaurant object.
 // Include name, location, rating, cuisine, and isOpen.
+
+type Restaurant = {
+    name: string,
+    location: string,
+    rating: number,
+    cuisine: string[],
+    isOpen: boolean
+};
+
+let restaurant_1: Restaurant = {
+    name: "ASMOLG",
+    location: "Hyderabad",
+    rating: 4.7,
+    cuisine: ["Paneer", "Tandoor", "Noodle"],
+    isOpen: true
+};
+
+console.log(restaurant_1);
+
+console.log('-----------------------------------------------------------------');
 
 // Q8
 // Create a laptop object.
 // Include brand, model, RAM, storage, processor, and price.
 
+type Laptop = {
+    brand: string,
+    model: string,
+    RAM: string,
+    storage: string,
+    processor: string,
+    price: number
+};
+
+let laptop_1: Laptop = {
+    brand: "Apple",
+    model: "MacBook Air M5",
+    RAM: "24 GB",
+    storage: "1 TB",
+    processor: "M5",
+    price: 137000
+};
+
+console.log(laptop_1);
+
+console.log('-----------------------------------------------------------------');
+
 // Q9
 // Create an object representing a course.
 // Include:
 // title, instructor, duration, price, isPublished.
+
+type Course = {
+    title: string,
+    instructor: string,
+    duration: number,
+    price: number,
+    isPublished: boolean
+};
+
+let course_1: Course = {
+    title: "AWS Basics",
+    instructor: "Saurabh Aralkar",
+    duration: 6.5,
+    price: 5000,
+    isPublished: true
+};
+
+console.log(course_1);
+
+console.log('-----------------------------------------------------------------');
 
 // Q10
 // Create a shoppingCart object.
@@ -158,16 +240,79 @@ console.log('-----------------------------------------------------------------')
 // customerName, totalItems, totalAmount.
 // Update totalItems and totalAmount.
 
+type shoppingCart = {
+    customerName: string,
+    totalItems: number,
+    totalAmount: number
+};
+
+let cart_1: shoppingCart = {
+    customerName: "Saurabh Bhai",
+    totalItems: 12,
+    totalAmount: 28000
+};
+
+console.log(cart_1);
+
+cart_1.totalItems = cart_1.totalItems + 4;
+cart_1.totalAmount = cart_1.totalAmount + 6000;
+
+console.log(cart_1);
+
+console.log('-----------------------------------------------------------------');
+
 // Q11
 // Create a company object.
 // Include:
 // name, industry, employees, revenue.
 // Increase the employee count by 50.
 
+type Company = {
+    name: string,
+    industry: string,
+    employees: number,
+    revenue: number
+};
+
+let company_1: Company = {
+    name: "Triroop Techconsult Pvt Ltd",
+    industry: "Information Technology",
+    employees: 4,
+    revenue: 1000000000
+};
+
+console.log(company_1);
+
+company_1.employees = company_1.employees + 50;
+
+console.log(company_1);
+
+console.log('-----------------------------------------------------------------');
+
 // Q12
 // Create a movie object.
 // Include:
 // title, director, releaseYear, rating, isReleased.
+
+type Movie = {
+    title: string,
+    director: string,
+    releaseYear: number,
+    rating: number,
+    isReleased: boolean
+};
+
+let movie_1: Movie = {
+    title: "Dhurandhar",
+    director: "Aditya Dhar",
+    releaseYear: 2025,
+    rating: 4.7,
+    isReleased: true
+};
+
+console.log(movie_1);
+
+console.log('-----------------------------------------------------------------');
 
 // Q13
 // Create a socialMediaProfile object.
@@ -175,13 +320,93 @@ console.log('-----------------------------------------------------------------')
 // username, followers, following, posts.
 // Increase followers by 100.
 
+type socialMediaProfile = {
+    username: string,
+    followers: number,
+    following: number,
+    posts: number
+};
+
+let profile_1: socialMediaProfile = {
+    username: "saurabh_aralkar",
+    followers: 417,
+    following: 312,
+    posts: 5
+};
+
+console.log(profile_1);
+
+profile_1.followers = profile_1.followers + 100;
+
+console.log(profile_1);
+
+console.log('-----------------------------------------------------------------');
+
 // Q14
 // Create an order object.
 // Include:
 // orderId, customerName, productName, quantity, price.
 // Calculate the total order value and store it in the object.
 
+type Order = {
+    orderId: string,
+    customerName: string,
+    productName: string,
+    quantity: number,
+    price: number,
+    totalAmount: number,
+};
+
+let order_1: Order = {
+    orderId: "1",
+    customerName: "Saurabh Aralkar",
+    productName: "MacBook Air",
+    quantity: 14,
+    price: 137000,
+    totalAmount: 0,
+};
+
+console.log(order_1);
+
+order_1.totalAmount = order_1.quantity * order_1.price;
+
+console.log(order_1);
+
+console.log('-----------------------------------------------------------------');
+
 // Q15
 // Create a real-world object of your choice.
 // It must contain at least 8 properties.
 // Read and modify at least 3 properties.
+
+type MyObject = {
+    name: string,
+    phone: number,
+    city: string,
+    isEmployeed: boolean,
+    income: number,
+    expense: number,
+    isMarried: boolean,
+    gender: string,
+};
+
+let myObject: MyObject = {
+    name: "Saurabh Aralkar",
+    phone: 9011694002,
+    city: "Hyderabad",
+    isEmployeed: true,
+    income: 29800,
+    expense: 27000,
+    isMarried: false,
+    gender: "Male"
+};
+
+console.log(myObject);
+
+myObject.income = myObject.income + 5000;
+myObject.expense = myObject.expense + 2000;
+myObject.isMarried = true;
+
+console.log(`Income: ${myObject.income}`);
+console.log(`Expense: ${myObject.expense}`);
+console.log(`Married: ${myObject.isMarried}`);
